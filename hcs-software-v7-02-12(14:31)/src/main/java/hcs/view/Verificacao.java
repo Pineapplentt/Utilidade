3a4eb16ca3f9625d72c4746c3bbc7befcfbda53e
GabrielVannucchi <gabriel.correa@sptech.school>
2022-12-02 14:41:39 -0300

    projeto individual

---
+++ hcs-software-v7-02-12(14:31)/src/main/java/hcs/view/Verificacao.java
@@ -0,0 +1,14 @@
+
+package hcs.view;
+
+public class Verificacao {
+    private static Integer intVerificacao;
+    
+    public static Integer getIntVerificacao() {
+        return intVerificacao;
+    }
+
+    public void setIntVerificacao(Integer intVerificacao) {
+        Verificacao.intVerificacao = intVerificacao;
+    }
+}
